237573544e71c1854143f19e773b3e770744adb5
jackiewaang <zjackiewang03@gmail.com>
2024-10-09 23:23:58 +0100

    Configured webpack with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.{ts,tsx,html,js}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      //'body':['Font']
+    }
+  },
+  plugins: [],
+}
+
